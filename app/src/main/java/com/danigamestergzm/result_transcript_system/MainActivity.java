6f2cd2740467040be860217a18a14320cd000133
GZMaster <danigamester@gmail.com>
2021-09-11 00:34:58 +0100

    Initial commit

---
+++ app/src/main/java/com/danigamestergzm/result_transcript_system/MainActivity.java
@@ -0,0 +1,14 @@
+package com.danigamestergzm.result_transcript_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
